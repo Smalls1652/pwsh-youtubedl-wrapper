3369cdf4a02523c5ef142d4c595e881c6c20cd6e
Timothy Small <smalls.online@icloud.com>
2020-08-05 23:16:52 -0400

    Added extra options to youtube-dl arguments

--- Invoke-YouTubeDL.ps1
+++ Invoke-YouTubeDL.ps1
@@ -3,19 +3,36 @@ param(
     [Parameter(Position = 0, Mandatory)]
     [string]$Video,
     [Parameter(Position = 1)]
-    [System.IO.DirectoryInfo]$OutputDirectory = ".\"
+    [System.IO.DirectoryInfo]$OutputDirectory = ".\",
+    [Parameter(Position = 2)]
+    [string]$ArchivePath
 )
 
 process {
+    $ytdlOptions = [System.Collections.Generic.List[string]]::new()
+    $ytdlOptions.Add("-f `"(bestvideo[ext=webm]+bestaudio[ext=webm]/bestvideo+bestaudio/best[ext=mp4]/best)`"")
+    $ytdlOptions.Add("--recode-video `"mp4`"")
+    $ytdlOptions.Add("-o `"%(title)s.%(ext)s`"")
+    $ytdlOptions.Add("--embed-subs")
+    $ytdlOptions.Add("--add-metadata")
+
+    switch ($null -eq $ArchivePath)
+    {
+        $false {
+            $ytdlOptions.Add("--download-archive `"$($ArchivePath)`"")
+            break
+        }
+
+        Default {
+            break
+        }
+    }
+
+    $ytdlOptions.Add("`"$($Video)`"")
+
     $YouTubeDlSplat = @{
         "FilePath"     = "youtube-dl";
-        "ArgumentList" = @(
-            "-f `"bestvideo+bestaudio`"",
-            "--embed-subs",
-            "--embed-thumbnail",
-            "--add-metadata",
-            "`"$($Video)`""
-        );
+        "ArgumentList" = $ytdlOptions;
         "Wait"         = $true;
         "NoNewWindow"  = $true;
     }
